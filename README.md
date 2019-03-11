df98571cd9a1ebc50992e0295e26b06259f2479d
zhaopeng <peng.zhao@togic.com>
2019-03-11 20:16:33 +0800

    VideoView实现视频播放

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MediaPlayer的简单实现
+## 播放生命周期
+[MediaPlayer生命周期](https://developer.android.com/images/mediaplayer_state_diagram.gif)
+
+## 常见问题
+1. VideoView播放短暂黑屏
+**产生原因**：
+视频文件加载入内存所需时间
+**解决方法**：
+(1)VideoView添加MediaPlayer.OnPreparedListener监听事件，并在onPrepared(MediaPlayer mp)回调方法中播放视频，此时视频已经完成了加载
+(2)VideoView执行start()方法后，延迟几百毫秒后开始播放
+
+2. VideoView播放时，切换跳转至其他界面后返回播放黑屏
+**产生原因**：
+VideoView被回收，且未作VideoView的状态保存
+**解决方法**：
+在VideoView所在的Activity或Fragment的生命周期中处理VideoView视频播放和暂停。
--- app/src/main/java/com/ziv/demo/mediaplayer/MainActivity.java
+++ app/src/main/java/com/ziv/demo/mediaplayer/MainActivity.java
@@ -2,13 +2,21 @@ package com.ziv.demo.mediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private String playUrl = "http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        videoView.setVideoPath(playUrl);
+        videoView.start();
+        MediaController mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
     }
 }
